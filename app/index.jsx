ef1511512736edb1efd4f41df1210bb7ecedbbad
aryanc193 <aryanc193@gmail.com>
2024-07-17 01:31:43 +0530

    made landing page

---
+++ app/index.jsx
@@ -0,0 +1,46 @@
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { useGlobalContext } from "../context/GlobalProvider";
+
+export default function App() {
+  // const { isLoading, isLogged } = useGlobalContext();
+
+  // if (!isLoading && isLogged) return <Redirect href="/home" />;
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[260px] h-[168px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover your nearest EV charging station with{" "}
+              <Text className="text-secondary-200">EVCS Finder App</Text>
+            </Text>
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Locate, know more and compare your nearest EV charging centers
+          </Text>
+
+          {/* <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          /> */}
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+}
